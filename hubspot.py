a9462b917b66332365c8ffecf2b0590a6ed4b5d0
Morgan West <43296702+morganzwest@users.noreply.github.com>
2023-10-10 11:18:57 +0100

    Add files via upload

--- hubspot.py
+++ hubspot.py
@@ -4,6 +4,33 @@ import requests
 import json
 import urllib.parse
 
+def update_hubspot_company_risk_factor(company_id, risk_factor, HUBSPOT_API_KEY):
+    # Endpoint to update a company
+    endpoint = f"https://api.hubapi.com/crm/v3/objects/companies/{company_id}"
+    
+    # Headers for the request
+    headers = {
+        'Authorization': f"Bearer {HUBSPOT_API_KEY}",
+        'Content-Type': 'application/json'
+    }
+    
+    # Data payload
+    data = {
+        "properties": {
+            "risk_factor": round(risk_factor, 2)  # Convert risk factor to string
+        }
+    }
+    
+    # Send the PATCH request
+    response = requests.patch(endpoint, headers=headers, data=json.dumps(data))
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        return True
+    else:
+        print(f"Error updating company {company_id}. Status code: {response.status_code}")
+        return False
+
 def fetch_companies_with_client_code(max_results=15000, HUBSPOT_API_KEY=None, limit=250):
     """
     Fetches a list of companies from HubSpot that have a client code.
@@ -27,7 +54,7 @@ def fetch_companies_with_client_code(max_results=15000, HUBSPOT_API_KEY=None, li
     # Parameters to send with each request
     parameter_dict = {
         'limit': limit,
-        'properties': ['name', 'client_code', 'status', 'hours_per_month', 'package_type', 'income_per_month']
+        'properties': ['name', 'companyId', 'domain', 'client_code', 'status', 'hours_per_month', 'package_type', 'income_per_month', 'rag_rating', 'original_clover_start_date']
     }
 
     # Headers for the request
@@ -45,6 +72,7 @@ def fetch_companies_with_client_code(max_results=15000, HUBSPOT_API_KEY=None, li
 
         # Make the GET request
         r = requests.get(url=get_url, headers=headers)
+        print(r.text)  # Add this right after the `r = requests.get(url=get_url, headers=headers)` line.
         response_dict = json.loads(r.text)
 
         # Update the flag for pagination based on response
@@ -65,17 +93,21 @@ def fetch_companies_with_client_code(max_results=15000, HUBSPOT_API_KEY=None, li
 
     for company in company_list:
         properties = company.get('properties', {})
+        company_id = company.get('companyId')  # Extract the companyId
 
-        # Only consider companies with a client_code
-        if 'client_code' in properties:
+        # Ensure that 'client_code' and 'status' properties exist
+        if 'client_code' in properties and 'status' in properties:
             try:
                 # Extract properties of each company
                 name = properties.get('name', {}).get('value', 'Unknown Name')
                 client_code = properties['client_code']['value']
                 status = properties['status']['value']
-                hours = properties['hours_per_month']['value']
-                package = properties['package_type']['value']
-                fee = properties['income_per_month']['value']
+                hours = properties.get('hours_per_month', {}).get('value', 0)
+                package = properties.get('package_type', {}).get('value', 'Unknown Package')
+                fee = properties.get('income_per_month', {}).get('value', 0)
+                start_date = properties.get('original_clover_start_date', {}).get('value', 'Unknown Date')
+                rag = properties.get('rag_rating', {}).get('value', 'Unknown RAG')
+                company_id = company_id
 
                 # Only add companies with 'Active' status to the list
                 if status == "Active":
@@ -85,7 +117,10 @@ def fetch_companies_with_client_code(max_results=15000, HUBSPOT_API_KEY=None, li
                             package,      # 1
                             name,         # 2
                             hours,        # 3
-                            fee           # 4
+                            fee,          # 4
+                            start_date, #5
+                            rag, #6 
+                            company_id #7
                         ]
                     )
             except KeyError:  # Skip the company if any property is missing
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, stream_with_context, Response
 import calendar, json
 from datetime import datetime, timedelta
-from hubspot import fetch_companies_with_client_code
+from hubspot import fetch_companies_with_client_code, update_hubspot_company_risk_factor
 from clockify import extract_summary, get_data_for_month
 from flask_simplelogin import SimpleLogin, login_required
+
 
 # Constants related to the Clockify API
 API_URL = "https://api.clockify.me/api/v1"
@@ -21,7 +22,50 @@ with open('config/hubspot_config.json', 'r') as f:
 HUBSPOT_API_KEY = hubspot_config["API_KEY"]
 
 app = Flask(__name__)
+
+# Set the consistent secret key
+app.config["SECRET_KEY"] = "122e7daa-5ca7-4a63-a289-c7af7df2c1da"
+
 SimpleLogin(app)
+
+def calculate_quartiles(data):
+    """
+    Calculate the first and third quartiles of a dataset.
+
+    :param data: List of numbers
+    :return: Q1, Q3
+    """
+    sorted_data = sorted(data)
+    n = len(sorted_data)
+
+    if n % 2 == 0:
+        median_pos = n // 2
+        lower_data = sorted_data[:median_pos]
+        upper_data = sorted_data[median_pos:]
+    else:
+        median_pos = n // 2
+        lower_data = sorted_data[:median_pos]
+        upper_data = sorted_data[median_pos + 1:]
+
+    Q1 = median(lower_data)
+    Q3 = median(upper_data)
+
+    return Q1, Q3
+
+def median(data):
+    """
+    Compute the median of a dataset.
+
+    :param data: List of numbers
+    :return: Median value
+    """
+    n = len(data)
+    mid = n // 2
+
+    if n % 2 == 0:
+        return (data[mid - 1] + data[mid]) / 2
+    else:
+        return data[mid]
 
 # Function to determine the color based on a comparison of two values
 def color_based_on_value(value, comparison):
@@ -54,7 +98,163 @@ def merge_project_and_client_data(projects_data, client_data):
                 )
     return merged_data
 
-@app.route('/')
+def calculate_risk_factors(hubspot_clients, averages, today):
+    client_code = client[0]
+    contracted_hours = float(client[3])
+    average_time = averages.get(client_code, 0)
+    
+    # RAG rating to value
+    rag_rating = client[6]
+    rag_value = {'Red': 0, 'Amber': 16.65, 'Green': 33.3}.get(rag_rating, 0)
+    
+    # Calculate actual hours usage percentage
+    usage_percentage = (average_time / contracted_hours) * 100 if contracted_hours != 0 else 0
+    if usage_percentage > 100: usage_percentage = 100
+    
+    # Months since start
+    start_date = datetime.fromtimestamp(int(client[5]) / 1000)
+    months_since_start = (today.year - start_date.year) * 12 + today.month - start_date.month - 3
+    if months_since_start > 33.3:
+        months_since_start = 33.3
+    elif months_since_start < 0:
+        months_since_start = 0
+
+    # Risk Factor calculation
+    risk_factor = rag_value + (usage_percentage / 3) + months_since_start
+    print("RISK:",client_code,rag_value,usage_percentage,months_since_start,risk_factor)
+    risk_factors[client_code] = risk_factor
+
+    return risk_factors
+
+def calculate_client_risk_factor(client, average_time, today):
+    client_code = client[0]
+    contracted_hours = float(client[3])
+    
+    # RAG rating to value
+    rag_rating = client[6]
+    rag_value = {'Red': 0, 'Amber': 16.65, 'Green': 33.3}.get(rag_rating, 0)
+
+    # Calculate actual hours usage percentage
+    usage_percentage = (average_time / contracted_hours) * 100 if contracted_hours != 0 else 0
+    if usage_percentage > 100: usage_percentage = 100
+    print(f"For client {client_code}: Average Time = {average_time}, Contracted Hours = {contracted_hours}, Calculated Usage = {usage_percentage}")
+
+    # Months since start
+    start_date = datetime.fromtimestamp(int(client[5]) / 1000)
+    months_since_start = (today.year - start_date.year) * 12 + today.month - start_date.month - 3
+    if months_since_start > 33.3:
+        months_since_start = 33.3
+    elif months_since_start < 0:
+        months_since_start = 0
+
+    # Risk Factor calculation
+    risk_factor = rag_value + (usage_percentage / 3) + months_since_start
+
+    print("RISK:",client_code,rag_value,usage_percentage,months_since_start,risk_factor)
+    
+    return risk_factor
+
+
+
+@app.route('/risk')
+@login_required
+def risk():
+    today = datetime.today()
+    year = today.year
+    month = today.month
+
+    # Decrement the month to skip the current month's data
+    month -= 1
+    if month == 0:  # If month becomes 0 (i.e., it was January), adjust to December of the previous year
+        month = 12
+        year -= 1
+    
+    # Fetching active clients from HubSpot
+    hubspot_clients = fetch_companies_with_client_code(HUBSPOT_API_KEY=HUBSPOT_API_KEY)
+
+    # Dictionary to store the data for the past six months for each client
+    six_month_data = {}
+    for client in hubspot_clients:
+        client_code = client[0]
+        six_month_data[client_code] = []
+
+    # Looping through the past six months to fetch and process data
+    for _ in range(6):
+        monthly_data = get_data_for_month(year, month)
+        summary_data = extract_summary(monthly_data)
+
+        # Extracting the actual time for each client
+        for client in hubspot_clients:
+            client_code = client[0]
+            actual_time = next((entry["actual_time"] for entry in summary_data if entry["project"].startswith(client_code)), 0)
+            six_month_data[client_code].append(actual_time)
+
+        # Adjusting the date for the next iteration
+        month -= 1
+        if month == 0:
+            month = 12
+            year -= 1
+
+    # Computing the average time for the past six months for each client
+    averages = {}
+    for client, times in six_month_data.items():
+        avg = sum(times) / 6
+        averages[client] = avg
+        print("Averages:",averages[client])
+
+    merged_data = []
+    for client in hubspot_clients:
+        client_code = client[0]
+        name = client[2]
+        monthly_fee = client[4]
+        rag = client[6]
+        average_time = averages.get(client_code, 0)
+        
+        # Calculate risk factor
+        risk_factor = calculate_client_risk_factor(client, average_time, today)
+        
+        # Calculate time variation (or any other metric for Percentage of Time Used)
+        contracted_hours = float(client[3])
+
+        # Calculate time variation (or any other metric for Percentage of Time Used)
+        time_variation = (average_time - contracted_hours) / contracted_hours * 100 if contracted_hours != 0 else 0
+        usage_percentage = (average_time / contracted_hours) * 100 if contracted_hours != 0 else 0
+
+        # Months as a client
+        start_date = datetime.fromtimestamp(int(client[5]) / 1000)
+        months_as_client = (today.year - start_date.year) * 12 + today.month - start_date.month
+
+        risk_factor = float(risk_factor)
+        monthly_fee = float(monthly_fee)
+
+        merged_data.append({
+            "code": client_code,
+            "name": name,
+            "risk_factor": risk_factor,
+            "monthly_fee": monthly_fee,
+            "usage": usage_percentage,
+            "rag": rag,
+            "months_as_client": months_as_client,
+            "safe_revenue": (risk_factor / 100) * monthly_fee
+        })
+
+    # Adjusting the month to display in the summary
+    month_to_display = today.month - 1
+    year_to_display = today.year
+    if month_to_display == 0:
+        month_to_display = 12
+        year_to_display -= 1
+    month_name = calendar.month_name[month_to_display]
+
+    merged_data.sort(key=lambda x: x['risk_factor'], reverse=True)
+
+    risk_factors = [entry['risk_factor'] for entry in merged_data]
+    Q1, Q3 = calculate_quartiles(risk_factors)
+
+    # Return the rendered template with the merged data
+    return render_template('risk.html', summary_data=merged_data, month_name=month_name, year=year_to_display, Q1=Q1, Q3=Q3)
+
+@app.route('/clockify')
 @login_required
 def summary():
     today = datetime.today()
@@ -69,6 +269,7 @@ def summary():
 
     # Fetching active clients from HubSpot
     hubspot_clients = fetch_companies_with_client_code(HUBSPOT_API_KEY=HUBSPOT_API_KEY)
+
 
     # Dictionary to store the data for the past six months for each client
     six_month_data = {}
@@ -112,6 +313,7 @@ def summary():
         avg = sum(times) / 6
         averages[client] = avg
 
+
     # Calculating the variation in time from the contracted hours for each client
     time_variations = {}
     for client in hubspot_clients:
@@ -119,6 +321,36 @@ def summary():
         contracted_hours = float(client[3])
         average_time = averages.get(client_code, 0)
         time_variations[client_code] = average_time - contracted_hours
+
+    risk_factors = {}
+    for client in hubspot_clients:
+        client_code = client[0]
+        contracted_hours = float(client[3])
+        average_time = averages.get(client_code, 0)
+        
+        # RAG rating to value
+        rag_rating = client[6]
+        rag_value = {'Red': 0, 'Amber': 16.65, 'Green': 33.3}.get(rag_rating, 0)
+        
+        # Calculate actual hours usage percentage
+        usage_percentage = (average_time / contracted_hours) * 100 if contracted_hours != 0 else 0
+        if usage_percentage > 100: usage_percentage = 100
+        
+        # Months since start
+        start_date = datetime.fromtimestamp(int(client[5]) / 1000)
+
+        months_since_start = (today.year - start_date.year) * 12 + today.month - start_date.month - 3
+        if months_since_start > 33.3:
+            months_since_start = 33.3
+        elif months_since_start < 0:
+            months_since_start = 0
+
+        # Risk Factor calculation
+        risk_factor = rag_value + (usage_percentage / 3) + months_since_start
+        print("RISK:",client_code,rag_value,usage_percentage,months_since_start,risk_factor)
+        risk_factors[client_code] = risk_factor
+        
+    print("Calculated risk factors:", risk_factors)
 
     # Merging the averaged data with the client data
     merged_data = []
@@ -131,17 +363,20 @@ def summary():
         reversed_monthly_data = list(six_month_data[client_code])
         reversed_monthly_data.reverse()
 
-        merged_data.append(
-            {
-                "code": client[0],
-                "package": client[1],
-                "name": client[2],
-                "contracted_hours": client[3],
-                "monthly_fee": client[4],
-                "time_variation": time_variation,
-                "monthly_data": reversed_monthly_data
-            }
-        )
+        if float(client[3]) != 0:
+            merged_data.append(
+                {
+                    "code": client[0],
+                    "package": client[1],
+                    "name": client[2],
+                    "contracted_hours": client[3],
+                    "monthly_fee": client[4],
+                    "time_variation": time_variation,
+                    "monthly_data": reversed_monthly_data,
+                    "risk_factor": risk_factors.get(client_code, 0),
+                    "id": client[7]
+                }
+            )        
 
     # Sorting the merged data by time variation in descending order
     merged_data.sort(key=lambda x: x['time_variation'], reverse=True)
@@ -156,8 +391,25 @@ def summary():
     
     last_six_months = get_last_six_months(today)
 
+    # for entry in merged_data:
+    #     company_id = entry['id']
+    #     risk_factor = entry['risk_factor']
+    #     update_hubspot_company_risk_factor(company_id, risk_factor, HUBSPOT_API_KEY)
+    #     print(f"POST: {entry['id']} - {risk_factor}")
+
+    for entry in merged_data:
+        if float(entry["contracted_hours"]) == 0:
+            print("Entry with zero contracted_hours:", entry)
+
     # Rendering the summary page with the computed data
-    return render_template('summary.html', summary_data=merged_data, months=last_six_months, month_name=month_name, year=year_to_display)
+    return render_template('clockify.html', summary_data=merged_data, months=last_six_months, month_name=month_name, year=year_to_display)
+
+logs = []
+
+@app.route('/')
+@login_required
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
